696f1aebaddf10b08e2f7cfd6aca121e0f5eff17
Platon <platon.tikhnenko@gmail.com>
2025-11-03 15:56:15 -0800

    installed django rest

--- MainService/MainService/settings.py
+++ MainService/MainService/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'community'
+    'community',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
