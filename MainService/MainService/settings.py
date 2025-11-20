f02835e5b01a87355a986a065cc2c91b7c1d6722
Platon <platon.tikhnenko@gmail.com>
2025-11-19 20:08:30 -0800

    allowed the gateway host

--- MainService/MainService/settings.py
+++ MainService/MainService/settings.py
@@ -27,7 +27,7 @@ API_GATEWAY_URL = 'http://localhost:5555/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://gateway_service:3333"]
 
 
 # Application definition
