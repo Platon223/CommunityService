78388852034e0d957d38c741ff766a11a1e616d2
Platon <platon.tikhnenko@gmail.com>
2025-11-20 15:42:53 -0800

    updated the allowed hosts variable

--- MainService/MainService/settings.py
+++ MainService/MainService/settings.py
@@ -27,7 +27,7 @@ API_GATEWAY_URL = 'http://localhost:5555/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://localhost:3333"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
